309fd19b0d8d69a0ac8fca1d1bf7e3dfda816414
Myssyb <beulahagiande@gmail.com>
2024-10-11 15:23:14 +0000

    dependencies installation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export function getListStudents(){
+	return [
+		{id:1, firstname:"Guillaume", location:"San Francisco",},
+		{id: 2, firstname:"Jame", location:"Serena",},
+		{id:3, firstname:"Serena", location:"San Francisco",}
+	]
