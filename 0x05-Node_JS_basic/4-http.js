0c63b1909c37f3fd5ceb0e92851ecd285f8aef1f
myssyb <beulahagiande@gmail.com>
2025-01-24 11:25:03 +0000

    Uodate

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,16 +5,13 @@ const HOST = 'localhost';
 const app = http.createServer();
 
 app.on('request', (_, res) => {
-	  const responseText = 'Hello Holberton School!';
-
-	  res.setHeader('Content-Type', 'text/plain');
-	  res.setHeader('Content-Length', responseText.length);
-	  res.statusCode = 200;
-	  res.write(Buffer.from(responseText));
+  const responseText = 'Hello ALX!';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
-
 app.listen(PORT, HOST, () => {
-	  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
-
 module.exports = app;
